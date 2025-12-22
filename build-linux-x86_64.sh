4f498d335406082c035c98ad8476834ee58a123c
Owain <30665433+AnMagician@users.noreply.github.com>
2025-12-22 16:16:51 +0000

    .

--- build-linux-x86_64.sh
+++ build-linux-x86_64.sh
@@ -26,10 +26,10 @@ echo "$LINUX_AMD64_CHKSUM linux64_jre.tar.gz" | sha256sum -c
 # Note: Host umask may have checked out this directory with g/o permissions blank
 chmod -R u=rwX,go=rX appimage
 # ...ditto for the build process
-chmod 644 build/libs/Project\ PK.jar
+chmod 644 build/libs/Project-PK.jar
 
-cp native/build-x64/src/Project\ PK build/linux-x64/
-cp build/libs/Project\ PK.jar build/linux-x64/
+cp native/build-x64/src/Project-PK build/linux-x64/
+cp build/libs/Project-PK.jar build/linux-x64/
 cp packr/linux-x64-config.json build/linux-x64/config.json
 cp build/filtered-resources/runelite.desktop build/linux-x64/
 cp appimage/runelite.png build/linux-x64/
--- build-win64.sh
+++ build-win64.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo Launcher sha256sum
-sha256sum build/libs/Project\ PK.jar
+sha256sum build/libs/Project-PK.jar
 
 cmake -S liblauncher -B liblauncher/build64 -A x64
 cmake --build liblauncher/build64 --config Release
@@ -25,7 +25,7 @@ fi
 echo "$WIN64_CHKSUM win64_jre.zip" | sha256sum -c
 
 cp native/build-x64/src/Release/Project\ PK.exe build/win-x64/
-cp build/libs/Project\ PK.jar build/win-x64/
+cp build/libs/Project-PK.jar build/win-x64/
 cp packr/win-x64-config.json build/win-x64/config.json
 cp liblauncher/build64/Release/launcher_amd64.dll build/win-x64/
 
